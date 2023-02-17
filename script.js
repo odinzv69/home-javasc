07c55b7c3655e1595307156994fb044926d1f83b
odinzv69 <125555046+odinzv69@users.noreply.github.com>
2023-02-17 20:27:16 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let a = 20;
+alert(a);
+
+let firstIphone = 2007;
+alert(firstIphone);
+
+let creatorJavascript = "Brendan Eich";
+alert(creatorJavascript);
+
+let x = 10;
+let y = 2;
+alert(x+y);
+alert(x-y);
+alert(x*y);
+alert(x/y);
+
+let result = 2**5;
+alert(result);
+
+let A = 9;
+let b = 2;
+alert(A%b);
