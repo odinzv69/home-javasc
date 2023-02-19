d5a79a44b71fdcc099166513dd771a01fc6dd02b
odinzv69 <125555046+odinzv69@users.noreply.github.com>
2023-02-19 07:59:35 +0300

    1 home work js

--- script.js
+++ script.js
@@ -20,3 +20,28 @@ alert(result);
 let A = 9;
 let b = 2;
 alert(A%b);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+let age = prompt('сколько вам лет?');
+alert(age);
+
+const user = {
+    name: String('Bob'),
+    age: Number(19),
+    isAdmin: Boolean(true),
+}
+user.cityOfResidence = String('Moscow');
+
+let info = prompt('Какую информацию хотите узнать о пользователе?', 'user');
+alert(user[info]);
+
+let admin= String(prompt('Введите ваше имя'));
+ alert(`Привет, ${admin} !`);
